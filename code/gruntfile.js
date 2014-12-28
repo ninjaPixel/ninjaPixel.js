f8e40bb55dc8eb8955c1ee3f4adb5e74152e3465
ninjaPixel <matthew@ninjaPixel.eu>
2014-12-28 13:34:37 +0000

    update build files

--- code/gruntfile.js
+++ code/gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function (grunt) {
-    var myDir = 'src3'; // this will change to 'src/' once branch work is complete
+    var myDir = 'src'; // this will change to 'src/' once branch work is complete
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -8,28 +8,28 @@ module.exports = function (grunt) {
                 //                separator: ';',
             },
             basic: {
-                src: [myDir + '/js/_chart.js', myDir + '/js/barChart.js', myDir + '/js/bubbleChart.js', myDir + '/js/lineChart.js' ],
+                src: ['src/js/_chart.js', 'src/js/barChart.js', 'src/js/bubbleChart.js', 'src/js/lineChart.js' ],
                 dest: 'dist/ninjaPixel.js',
             },
             bundle: {
-                src: ['node_modules/d3/d3.js', '../../d3-tip/index.js', 'node_modules/moment/moment.js', 'dist/ninjaPixel.js', myDir + '/js/bubbleChart.js', myDir + '/js/lineChart.js'],
+                src: ['node_modules/d3/d3.js', 'src/js/d3.tip.js', 'dist/ninjaPixel.js'],
                 dest: 'dist/ninjaPixel.bundle.js',
             }
         },
         ts: {
             default: {
-                src: [myDir + '/*.ts', '!' + myDir + '/*.d.ts'],
+                src: ['src/*.ts', '!src/*.d.ts'],
                 options: {
                     declaration: false, // set to true to crete .d.ts files
-                    sourceMap: false // set to true to create map files
+                    sourceMap:   false // set to true to create map files
                 },
-                outDir: myDir + '/js'
+                outDir: 'src/js'
             }
         },
         rename: {
             moveDefinitions: {
-                src: myDir + '/js/*.d.ts',
-                dest: myDir + '/typescript_definitions/'
+                src:  'src/js/*.d.ts',
+                dest: 'src/typescript_definitions/'
             }
         }
     });
@@ -41,6 +41,4 @@ module.exports = function (grunt) {
 
     // Default task(s).
     grunt.registerTask('default', ['ts', 'concat']);
-    //    grunt.registerTask('default', ['ts', 'rename']);
-
 };
